b8e1f8825e41d4226f557d767928e400f7be6337
Rick <rickpowell1311@gmail.com>
2017-01-27 10:06:46 +0000

    Add entity to solution when creating with CrmPlusPlusCustomizationClient

--- Civica.CrmPlusPlus.Sdk/Client/CrmPlusPlusCustomizationClient.cs
+++ Civica.CrmPlusPlus.Sdk/Client/CrmPlusPlusCustomizationClient.cs
@@ -7,6 +7,7 @@ using Civica.CrmPlusPlus.Sdk.DefaultEntities;
 using Civica.CrmPlusPlus.Sdk.EntityAttributes;
 using Civica.CrmPlusPlus.Sdk.EntityAttributes.PropertyTypes;
 using Civica.CrmPlusPlus.Sdk.Validation;
+using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Messages;
 using Microsoft.Xrm.Sdk.Metadata;
@@ -53,7 +54,16 @@ namespace Civica.CrmPlusPlus.Sdk.Client
                 }
             };
 
-            service.Execute(createEntityRequest);
+            var response = (CreateEntityResponse)service.Execute(createEntityRequest);
+
+            var addReq = new AddSolutionComponentRequest()
+            {
+                ComponentType = (int)SolutionComponentTypes.Entity,
+                ComponentId = response.EntityId,
+                SolutionUniqueName = Solution.Name
+            };
+            
+            service.Execute(addReq);
         }
 
         public void CreateProperty<T, TProperty>(Expression<Func<T, TProperty>> propertyExpr) where T : CrmPlusPlusEntity, new()
