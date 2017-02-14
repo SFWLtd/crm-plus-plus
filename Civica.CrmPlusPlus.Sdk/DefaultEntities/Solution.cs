c8cd884cc84d129abfb2bffe150a18b17066990c
Rick Powell <richard.powell@sfwltd.co.uk>
2017-02-14 12:38:02 +0000

    Add IsManaged and IsVisible properties to default solution entity

--- Civica.CrmPlusPlus.Sdk/DefaultEntities/Solution.cs
+++ Civica.CrmPlusPlus.Sdk/DefaultEntities/Solution.cs
@@ -22,6 +22,16 @@ namespace Civica.CrmPlusPlus.Sdk.DefaultEntities
         [String(100, EntityAttributes.Metadata.StringFormatName.Text)]
         public string Version { get; set; }
 
+        [PropertyName("ismanaged")]
+        [PropertyInfo("Is Managed", EntityAttributes.Metadata.AttributeRequiredLevel.ApplicationRequired)]
+        [Boolean]
+        public bool IsManaged { get; set; }
+
+        [PropertyName("isvisible")]
+        [PropertyInfo("Is Visible", EntityAttributes.Metadata.AttributeRequiredLevel.ApplicationRequired)]
+        [Boolean]
+        public bool IsVisible { get; set; }
+
         [PropertyName("publisherid")]
         [PropertyInfo("Publisher", EntityAttributes.Metadata.AttributeRequiredLevel.ApplicationRequired)]
         [Lookup]
